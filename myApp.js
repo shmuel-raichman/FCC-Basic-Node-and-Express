48051dc6ceeede1bad4de6eed2e68aed64107227
Glitch (smuel1414-node-start) <none>
2019-08-07 17:22:34 +0000

    🍅🚠 Updated with Glitch Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -5,7 +5,10 @@ var app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 // --> 7)  Mount the Logger middleware here
-
+app.use( function(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
 
