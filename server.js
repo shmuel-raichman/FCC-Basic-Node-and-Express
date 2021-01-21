72655b0da46b0b65996513e9aaf70a2bc1202157
Shmulik <smuel1414@gmail.com>
2021-01-21 13:36:06 +0200

    Fix - git log only one commit

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use('/', myApp);
-app.listen(port, () => console.log('Gator app listening on port ', port));
+app.listen(port, () => console.log('App listening on port ', port));
 
